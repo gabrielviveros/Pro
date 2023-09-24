6e76b3e753a4fea0bf611e3264c555387855db83
gabrielviveros <97645126+gabrielviveros@users.noreply.github.com>
2023-09-24 15:17:05 -0300

    Add files via upload

---
+++ Ejercicio_Clases2.py
@@ -0,0 +1,59 @@
+class Cursos():
+    fechaDeInicio=
+    titulo=""
+    descripcion=""
+    objetivos=""
+    programa=""
+    costo=""
+    duracion=0
+    fotos=""
+    
+class Estado():
+    disponible=""
+    noDisponible=""
+
+class Categoria():
+    inicial=""
+    intermedio=""
+    avanzado=""
+    
+class Clases():
+    fecha=""
+    titulo=""
+    contenido=""
+    url=""
+    
+    
+class Usuario():
+    apellido=""
+    nombre=""
+    dni=0
+    fechaDeNacimiento=0
+    direccion=""
+    localidad=""
+    provincia=""
+    codigoPostal=0
+    numeroDeCelular=0
+    email=""
+    claveDeAcceso=""
+    permisos=""
+    
+class Docente(Usuario):
+    
+    
+class Alumno(Usuario):
+       confirmarClaveDeAcceso=""
+    
+class Administrador(Usuario)
+    
+   
+    
+    
+    def __init__(self, NombreEscuela, Direccion, Telefono, CuerpoDirectivo, Especialidad):
+        self.nombreEscuela=NombreEscuela
+        self.direccion=Direccion
+        self.telefono=Telefono
+        self.cuerpoDirectivo=CuerpoDirectivo
+        self.especialidad=Especialidad
+        
+        
---
+++ Estructuras_Condicionales2.py
@@ -0,0 +1,91 @@
+# 1 Estructuras Condicionales}
+# print("Bienvenido!")
+# numero = input("Por favor! Ingrese un numero a continuacion: ")
+# if numero == "10":
+#   print("¡Usted ha ganado el sorteo!")
+      
+ #2 suma
+# numero = int (input("Ingrese un numero, por favor: "))
+# if numero == 10:
+#     print("¡Usted ha ganado el sorteo!")
+# elif numero > 10:
+#     print ("¡Te pasaste, a seguir intentando!")
+    
+# else:
+#     print("¡Falto un poco, seguí participando!")
+    
+ #3  pedir que ingrese un dia de la semana
+# dia = input ("Ingrese un dia de la semana: ")
+# if dia == "lunes":
+#   print ("Hoy es el primer dia de la semana a ponerle pilas")
+  
+# elif dia == "viernes":
+#   print ("Es el ultimo dia laboral o escolar de la semana")
+  
+# elif dia == "sabado":
+#   print ("A descansar que es Sabado!")
+  
+# elif dia == "domingo":
+#   print ("Hoy se come asado!!!")
+  
+# else:
+#   print ("Vamos que nos falta menos para el fin de semana!!")
+
+
+#4 solicite letra
+# letra = input("Hola, por favor ingrese una letra: ")
+# if letra ==  "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
+#   print ("Es vocal")
+  
+###### ejercicios estructuras repetidas   
+## 1 suma
+print ("Ingrese los numeros que desee sumar (-1 para finalizar)")
+suma = 0
+while True:
+  numero = int (input("Ingrese un numero, por favor: "))
+  if numero == -1:
+    break
+  suma += numero
+     
+  print ("El resultado es: ", suma + numero);
+ 
+
+# #2 que pida numeros
+# numero = int (input("Ingrese un numero que desee por favor: "))
+# a = 0
+# b = 0
+# c = 0
+
+# 
+
+
+#3 vocal
+# while True:
+#   letra =input ("Ingrese una letra (0 para salir): ")
+#   if letra == '0':
+#     break
+#   elif letra in 'aeiou':
+#     print("VOCAL")
+#   else:
+#     print("NO VOCAL")
+
+# ingresar numeros
+# suma = 0
+# contador = 0
+
+
+# while True:
+#   numero = int(input("Ingrese un numero (0 para salir): "))
+#   if numero == 0:
+#     break
+#   suma += numero
+#   contador += 1
+  
+  
+# if contador > 0:
+#   promedio = suma / contador
+#   print("suma: ", suma)
+#   print("contador: ", promedio)
+  
+
+  
